5db8b7031b8d1247825050a60a70899d91e4f6d6
Mikhail19870105 <crazyhtc87@gmail.com>
2020-05-28 13:06:39 +0300

    Update Conditioner.java

--- src/main/java/ru/netology/domain/Conditioner.java
+++ src/main/java/ru/netology/domain/Conditioner.java
@@ -1,4 +1,56 @@
 package ru.netology.domain;
 
 public class Conditioner {
+    private int maxTemperature;
+    private int minTemperature;
+    private int currentTemperature;
+
+    public int getMaxTemperature() {
+        return maxTemperature;
+    }
+
+    public int getMinTemperature() {
+        return minTemperature;
+    }
+
+    public void setMaxTemperature(int maxTemperature) {
+        this.maxTemperature = maxTemperature;
+    }
+
+    public void setMinTemperature(int minTemperature) {
+        this.minTemperature = minTemperature;
+    }
+
+    public int getCurrentTemperature() {
+        return currentTemperature;
+    }
+
+    public void setCurrentTemperature(int currentTemperature) {
+        if (currentTemperature > maxTemperature) {
+            this.currentTemperature = maxTemperature;
+            return;
+        }
+        if (currentTemperature < minTemperature){
+            this.currentTemperature = minTemperature;
+        return;
+        }
+        this.currentTemperature = currentTemperature;
+    }
+
+    public void increaseCurrentTemperature() {
+        if (currentTemperature >= maxTemperature) {
+            return;
+        }
+        this.currentTemperature++;
+
+    }
+
+    public void decreaseCurrentTemperature() {
+        if (currentTemperature <= minTemperature){
+            return;
+        }
+        this.currentTemperature--;
+
+    }
+
 }
